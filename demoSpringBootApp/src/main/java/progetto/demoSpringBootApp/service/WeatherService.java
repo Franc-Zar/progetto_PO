65453a927fcf8006675382468af92f52d3e1167e
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-09 12:38:40 +0100

    Delete WeatherService.java

--- demoSpringBootApp/src/main/java/progetto/demoSpringBootApp/service/WeatherService.java
+++
@@ -1,9 +0,0 @@
-package progetto.demoSpringBootApp.service;
-
-import java.util.Vector;
-import progetto.demoSpringBootApp.model.CityDataExt;
-
-public interface WeatherService {
-
-	public abstract Vector<CityDataExt> actualService(double lat,double lon,int cnt);
-}
