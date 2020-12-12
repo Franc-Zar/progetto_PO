da108be8473b94bc62185f0ba2428dc538673ff0
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-12 16:38:01 +0100

    Add files via upload

--- ArchiveStarter.java
+++ ArchiveStarter.java
@@ -4,35 +4,63 @@ package progetto.demoSpringBootApp.service;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Vector;
 import java.io.IOException;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class ArchiveStarter {
 
 	private final String nomeFile="src/main/resources/historical.txt";
 	
-	public JSONArray putArchive() {
+	public String putArchive(double lat, double lon, int cnt, int period) {
+		
+		
+		
+		
+	
+	}
+
+
+
+private Vector<String> citiesInCircle(double lat, double lon, int cnt) {
+	
+	
+	
+	
+	
+	
+	return
+}
+
+
+private String getCityArchive (String fileId, int period) {
+	
+	
+	
+	
+	
+	
+}
+
+
+private String readFile (String nomeFile) {
+	String archive="";
+	try {
+		
+		BufferedReader reader= new BufferedReader(new FileReader(nomeFile));
 		
 		try {
 		
-			BufferedReader reader= new BufferedReader(new FileReader(nomeFile));
-			String archive="";
+			String app="";
 			
-			try {
-			
-				String app="";
-				
-				while((app=reader.readLine())!=null) archive+=app;
-			
-			} catch(IOException e) {System.out.println("ERROR: I/O error while reading file");}
-			
-			//JSONObject obj = new JSONObject(archive);
-			return new JSONArray(archive);
+			while((app=reader.readLine())!=null) archive+=app;
 		
-		} catch (FileNotFoundException e) {System.out.println("ERROR: FILE not found");;}
-		
-		return new JSONArray("[{\"file\":\"nontrovato\"}]");
-	}
+		} catch(IOException e) {System.out.println("ERROR: I/O error while reading file");}
+	
+	  
+} catch(FileNotFoundException e) {System.out.println("ERROR: file not found");}
+	
+	 return archive;
+	
+}
+
 }
