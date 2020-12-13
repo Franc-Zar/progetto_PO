6446a89443ae6faab96715705848ea303f3b489f
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-13 11:01:21 +0100

    Add files via upload

--- ArchiveStarter.java
+++ ArchiveStarter.java
@@ -1,9 +1,6 @@
 
 package progetto.demoSpringBootApp.service;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,7 +9,7 @@ import java.io.IOException;
 
 public class ArchiveStarter {
 
-	private final String nomeFile="src/main/resources/historical.txt";
+	//private final String nomeFile="src/main/resources/historical.txt";
 	
 	public String putArchive(double lat, double lon, int cnt, int period) {
 		
@@ -23,54 +20,49 @@ public class ArchiveStarter {
 	}
 
 
-
-private final String pathElenco="";
-private final double limiteCerchio=2.5;
-private final double passo=0.01;
-
+/*
 private Vector<String> citiesInCircle(double lat, double lon, int cnt) {
-	String archive="";
-	try {
-		BufferedReader reader= new BufferedReader(new FileReader(pathElenco));
 	
-		try {
-			String app="";		
-			while((app=reader.readLine())!=null) archive+=app;
-		} catch(IOException e) {System.out.println("ERROR: I/O error while reading file");}
-  
-	} catch(FileNotFoundException e) {System.out.println("ERROR: file not found");}
-
-	JSONArray elenco= new JSONArray(archive);
-	Vector<String> cities=null;
-	double latCitta,lonCitta;
-	int prese=0;
 	
-	for(int n=0; n<limiteCerchio; n++) {
-		for(int i=0; i<elenco.length(); i++) {
-			JOSNObject o=elenco.getJSONObject(i);
-			latCitta=o.getDouble("lat");
-			lonCitta=o.getDouble("lon");
-			
-			if((latCitta>lat-n*passo)&&(latCitta<lat+n*passo))
-				if((lonCitta>lon-n*passo)&&(lonCitta<lon+n*passo)) {
-					cities.add(o.getString("fileID"));
-					elenco.remove(i);
-					prese++;
-					if(prese==cnt) return cities;
-				}
-		}
-	}	
-	return cities;
+	
+	
+	
+	
+	return
 }
-	
+
+*/
 private String getCityArchive (String fileId, int period) {
 	
+ 
+	String archive="";
+	try {
+		
+		BufferedReader reader= new BufferedReader(new FileReader("src/main/resources/"+fileId));
+		
+		try {
+		    int count=0;
+			String line="";
+			
+			while((line=reader.readLine())!=null && count<period+2) {
+				archive+=line;
+				
+				if(line==null && count<period+2) throw new ArchiveException("ERROR: Archive has no stats available "
+						+ "for requested period");
+			}
+		
+			
+		} catch(IOException e) {System.out.println("ERROR: I/O error while reading file");}
 	
+	  
+} catch(FileNotFoundException e) {System.out.println("ERROR: file not found");}
 	
+	 return archive;
 	
-	
-	
+
 }
+
+
 
 
 }
