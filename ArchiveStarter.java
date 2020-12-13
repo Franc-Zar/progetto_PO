2c2a02df5d56e4e55316b283d4e094b81d4fc1e4
Luigi-S <75085145+Luigi-S@users.noreply.github.com>
2020-12-13 12:23:08 +0100

    Update ArchiveStarter.java

--- ArchiveStarter.java
+++ ArchiveStarter.java
@@ -7,6 +7,14 @@ import java.io.FileReader;
 import java.util.Vector;
 import java.io.IOException;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+private final String pathElenco="";
+private final double limiteCerchio=2.5;
+private final double passo=0.01;
+
+
 public class ArchiveStarter {
 
 	//private final String nomeFile="src/main/resources/historical.txt";
@@ -20,18 +28,43 @@ public class ArchiveStarter {
 	}
 
 
-/*
+
 private Vector<String> citiesInCircle(double lat, double lon, int cnt) {
+	String archive="";
+	try {
+		BufferedReader reader= new BufferedReader(new FileReader(pathElenco));
 	
-	
-	
-	
-	
-	
-	return
+		try {
+			String app="";		
+			while((app=reader.readLine())!=null) archive+=app;
+		} catch(IOException e) {System.out.println("ERROR: I/O error while reading file");}
+  
+	} catch(FileNotFoundException e) {System.out.println("ERROR: file not found");}
+
+	JSONArray elenco= new JSONArray(archive);
+	Vector<String> cities=null;
+	double latCitta,lonCitta;
+	int prese=0;
+	double limite= limiteCerchio/passo;
+	for(int n=0; n<limite; n++) {
+		for(int i=0; i<elenco.length(); i++) {
+			JSONObject o=elenco.getJSONObject(i);
+			latCitta=o.getDouble("lat");
+			lonCitta=o.getDouble("lon");
+			
+			if((latCitta>=lat-n*passo)&&(latCitta<=lat+n*passo))
+				if((lonCitta>=lon-n*passo)&&(lonCitta<=lon+n*passo)) {
+					cities.add(o.getString("fileID"));
+					elenco.remove(i);
+					prese++;
+					if(prese==cnt) return cities;
+				}
+		}
+	}	
+	return cities;
 }
 
-*/
+	
 private String getCityArchive (String fileId, int period) {
 	
  
