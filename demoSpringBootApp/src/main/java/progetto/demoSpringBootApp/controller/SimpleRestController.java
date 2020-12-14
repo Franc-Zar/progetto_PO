fd6e95cf6514046596df703aa104224cf8e2eb3b
Luigi-S <75085145+Luigi-S@users.noreply.github.com>
2020-12-14 10:00:35 +0100

    Update SimpleRestController.java

--- demoSpringBootApp/src/main/java/progetto/demoSpringBootApp/controller/SimpleRestController.java
+++ demoSpringBootApp/src/main/java/progetto/demoSpringBootApp/controller/SimpleRestController.java
@@ -37,22 +37,4 @@ public class SimpleRestController {
 		return weatherService.statService(period,lat,lon,cnt);
 		
 	}
-	/*
-	@GetMapping("/stats/pressure")
-	/*
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public CityData example1(@RequestParam(name="name",defaultValue="Enzino") String param1) {
-		return new CityData(param1,"Iacchetti");
-	}
-	@GetMapping("/hello/{param1}")
-	public CityData example3(@PathVariable String param1) {
-		return new CityData(param1,"Iacchetti");
-	}
-	
-	@PostMapping("/hello/{param1}")
-	public CityData example2(@PathVariable String param1,@RequestBody String body) {
-		return new CityData(param1,body);
-	}
-	
-	*/
 }
