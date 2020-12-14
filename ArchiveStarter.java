a1fe230b2483825e78fd7fc4ee941032c346a1a1
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-14 17:07:07 +0100

    Add files via upload

--- ArchiveStarter.java
+++ ArchiveStarter.java
@@ -2,29 +2,40 @@
 package progetto.demoSpringBootApp.service;
 
 import java.io.BufferedReader;
+
+import progetto.demoSpringBootApp.customExceptions.ArchiveException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Vector;
 import java.io.IOException;
+import java.util.Vector;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-private final String pathElenco="";
-private final double limiteCerchio=2.5;
-private final double passo=0.01;
-
-
 public class ArchiveStarter {
-
-	//private final String nomeFile="src/main/resources/historical.txt";
 	
 	public String putArchive(double lat, double lon, int cnt, int period) {
 		
+		if(period<1) period = 1;
 		
-		
-		
+		Vector<String> circle = new Vector<String>();
+		String archive= "[";
 	
+		circle = citiesInCircle(lat,lon,cnt);
+		
+		for(int i=0; i<circle.size();i++)
+			try {
+				
+				archive+=getCityArchive(circle.get(i), period);
+				
+				if(i<circle.size()-1) archive+= ",";
+				
+				if(i==circle.size()-1) archive+="]";
+				
+			} catch (ArchiveException e) {System.out.println(e);}
+		
+		
+		return archive;		
 	}
 
 
@@ -32,7 +43,7 @@ public class ArchiveStarter {
 private Vector<String> citiesInCircle(double lat, double lon, int cnt) {
 	String archive="";
 	try {
-		BufferedReader reader= new BufferedReader(new FileReader(pathElenco));
+		BufferedReader reader= new BufferedReader(new FileReader("src/main/resources/CityList"));
 	
 		try {
 			String app="";		
@@ -41,10 +52,12 @@ private Vector<String> citiesInCircle(double lat, double lon, int cnt) {
   
 	} catch(FileNotFoundException e) {System.out.println("ERROR: file not found");}
 
-	JSONArray elenco= new JSONArray(archive);
-	Vector<String> cities=null;
+	JSONArray elenco = new JSONArray(archive);
+	Vector<String> cities= new Vector<String>();
 	double latCitta,lonCitta;
 	int prese=0;
+	double limiteCerchio=2.5;
+	double passo=0.1;		
 	double limite= limiteCerchio/passo;
 	for(int n=0; n<limite; n++) {
 		for(int i=0; i<elenco.length(); i++) {
@@ -54,7 +67,7 @@ private Vector<String> citiesInCircle(double lat, double lon, int cnt) {
 			
 			if((latCitta>=lat-n*passo)&&(latCitta<=lat+n*passo))
 				if((lonCitta>=lon-n*passo)&&(lonCitta<=lon+n*passo)) {
-					cities.add(o.getString("fileID"));
+					cities.add(o.getString("fileId"));
 					elenco.remove(i);
 					prese++;
 					if(prese==cnt) return cities;
@@ -63,9 +76,10 @@ private Vector<String> citiesInCircle(double lat, double lon, int cnt) {
 	}	
 	return cities;
 }
-
 	
-private String getCityArchive (String fileId, int period) {
+
+
+private String getCityArchive (String fileId, int period) throws ArchiveException {
 	
  
 	String archive="";
@@ -80,10 +94,20 @@ private String getCityArchive (String fileId, int period) {
 			while((line=reader.readLine())!=null && count<period+2) {
 				archive+=line;
 				
-				if(line==null && count<period+2) throw new ArchiveException("ERROR: Archive has no stats available "
-						+ "for requested period");
-			}
+				if(count==period+1) { 
+					
+					archive = archive.substring(0, archive.length()-1);
+					
+					archive+= "]}";
+				}
+				
+				count++;
+		}
 		
+			reader.close();
+			
+		if(count<period+2) throw new ArchiveException("ERROR: Archive has no stats available "
+						+ "for requested period");
 			
 		} catch(IOException e) {System.out.println("ERROR: I/O error while reading file");}
 	
