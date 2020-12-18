7fef3c893081005f132ee08f4b847499d13e3f61
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-18 08:43:23 +0100

    Update OpenWeather.java

--- OWAPI/src/main/java/com/progettoOOP/OWAPI/service/OpenWeather.java
+++ OWAPI/src/main/java/com/progettoOOP/OWAPI/service/OpenWeather.java
@@ -1,11 +1,5 @@
 package com.progettoOOP.OWAPI.service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,10 +7,8 @@ import java.util.Arrays;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.progettoOOP.OWAPI.util.FileUtilities;
 import com.progettoOOP.OWAPI.model.AbstractCityData;
-import com.progettoOOP.OWAPI.model.CityData;
-import com.progettoOOP.OWAPI.model.CityDataStats;
-import com.progettoOOP.OWAPI.model.maxVarianceCity;
 
 /* @Author Francesco Zaritto
  * @Author Luigi Smargiassi 
@@ -27,7 +19,7 @@ import com.progettoOOP.OWAPI.model.maxVarianceCity;
 public class OpenWeather {
 	
 	//key necessaria per chiamare l'API
-	private final String APIkey="bd0426afbb623d69f91f5c3f70b6613d";
+	private final static String APIkey="bd0426afbb623d69f91f5c3f70b6613d";
 
 /* metodo public che prende in input le coordinate geografiche (lat,lon), il periodo entro il quale definire
  * i dati (period=0 nel caso di informazioni attuali), il numero totale di città da analizzare (cnt = 1 default)
@@ -35,7 +27,7 @@ public class OpenWeather {
  * pressure/cloud nel caso di richieste di statistiche). Restituisce un ArrayList contenente le città cercate
  * e le relative informazioni desiderate
  */
-	public ArrayList<AbstractCityData> APIcall(double lat,double lon,int period,int cnt,String type){
+	public static ArrayList<AbstractCityData> APIcall(double lat,double lon,int period,int cnt,String type){
 
 		if(cnt<1||cnt>20) throw new IllegalArgumentException("ERROR: Invalid cities' number");
 		if(lon<-180.0||lon>180.0) throw new IllegalArgumentException("ERROR: Invalid longitude");
@@ -49,21 +41,21 @@ public class OpenWeather {
             +"&lon="+lon+"&cnt="+cnt+"&units=metric&appid="+APIkey;
 		else site="http://localhost:8080/archive/"+period+"?lat="+lat+"&lon="+lon+"&cnt="+cnt;
 		
-		String json=getFileContent(site);
+		String json=FileUtilities.getSiteContent(site);
 		JSONArray data;
 		JSONObject obj;
 
 		if(type.equals("actual")) {
 			obj = new JSONObject(json);
 			data = obj.getJSONArray("list");
-			filterData(data, filteredData);
+			Filters.filterData(data, filteredData);
 		}
 		else if(type.equals("pressure") || type.equals("cloud")) {	
 			data = new JSONArray(json);
 			getStats(data,filteredData,type,period);
 			Arrays.asList(filteredData);
 			Collections.sort(filteredData);
-			findMaxVariance(filteredData);
+			Filters.findMaxVariance(filteredData);
 			
 		}
 		else if(type.equals("all")) {
@@ -82,95 +74,11 @@ public class OpenWeather {
 	 * effettuare le statistiche (type = pressure/cloud) e il periodo (period) sul quale calcolare tali statistiche
 	 * (media e varianza) 
 	 */
-	private void getStats(JSONArray data, ArrayList<AbstractCityData> filteredData, String type, int period) {
-		
+	private static void getStats(JSONArray data, ArrayList<AbstractCityData> filteredData, String type, int period) {
 		
 	if(type.equals("all")) Filters.calculateStats(data, filteredData, period, "cloud","pressure");
 		
 	   else Filters.calculateStats(data, filteredData, period, type);
 	   
-	 
+	 }
 }
-
-/* metodo private che prende come parametro un JSONArray contenente i dati ottenuti dalla chiamata all'API 
- * (data) e un ArrayList (filterdData). Il metodo popola "filteredData" con le città cercate e i 
- * corrispondenti valori di pressione e nuvolosità, filtrati dalla totalità di informazioni fornite dall'API
- */
-	private void filterData(JSONArray data, ArrayList<AbstractCityData> filteredData) {
-		String name;
-		double lon,lat;
-		int pressure,clouds;
-		
-		for(int i=0; i<data.length();i++) {	
-			name = data.getJSONObject(i).getString("name");
-			lon = data.getJSONObject(i).getJSONObject("coord").getDouble("lon");
-			lat = data.getJSONObject(i).getJSONObject("coord").getDouble("lat");
-			pressure = data.getJSONObject(i).getJSONObject("main").getInt("pressure");
-			clouds = data.getJSONObject(i).getJSONObject("clouds").getInt("all");
-			filteredData.add(new CityData(lat,lon,name,clouds,pressure));
-		}	
-	}
-
-
-/* metodo private che prende come parametro l'url di una pagina web, apre la connessione e restituisce 
- * il contenuto della pagina sotto formato di Stringa  
- */
-	private String getFileContent(String site) {
-		HttpURLConnection connection = null;
-		try {
-			URL url=new URL(site);
-        	connection = (HttpURLConnection) url.openConnection();
-        	connection.setConnectTimeout(5000);
-			connection.connect();
-			connection.setReadTimeout(10000);
-		}catch(MalformedURLException e) { e.printStackTrace(); }
-		catch(IOException a) { a.printStackTrace(); }
-		
-		String json="";
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));	
-			String appoggio;
-			while((appoggio=reader.readLine())!=null)
-				json+=appoggio;
-			reader.close();
-		} catch (IOException e) {e.printStackTrace();}
-		return json;
-	}
-
-	
-/* metodo private che prende come parametro una ArrayList di dati ordinati rispetto alla media di 
- * nuvolosità/pressione e la restituisce dopo aver aggiunto, come ultimo elemento, la città che presenta
- * varianza massima di nuvolosità/pressione
- */
-	private void findMaxVariance(ArrayList<AbstractCityData> filteredData) {
-		
-		double maxVariance = ((CityDataStats) filteredData.get(0)).getVariance();
-        double lat = ((CityDataStats) filteredData.get(0)).getLat();
-	    double lon = ((CityDataStats) filteredData.get(0)).getLon();
-	    double newVariance;
-	      
-        String name = ((CityDataStats) filteredData.get(0)).getName();
-		
-		for(int i=0; i<filteredData.size(); i++) {
-			     
-			newVariance = ((CityDataStats) filteredData.get(i)).getVariance();
-			
-			            if(maxVariance < newVariance) {
-			            	
-			            	     maxVariance = newVariance;
-			            	     name = ((CityDataStats) filteredData.get(i)).getName();
-			            	     lat = ((CityDataStats) filteredData.get(i)).getLat();
-			            	     lon = ((CityDataStats) filteredData.get(i)).getLon();
-			          } }
-	
-		filteredData.add(new maxVarianceCity(lat,lon,name,maxVariance));
-		
-		}           	                          
-			
-	}
-	
-
-
-
-	
