c0699041049eb53ab7a25fa96f8568f05e528150
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-18 09:28:49 +0100

    Update RequestBodyClass.java

--- OWAPI/src/main/java/com/progettoOOP/OWAPI/model/RequestBodyClass.java
+++ OWAPI/src/main/java/com/progettoOOP/OWAPI/model/RequestBodyClass.java
@@ -1,7 +1,45 @@
 package com.progettoOOP.OWAPI.model;
 
 public class RequestBodyClass {
-	public double lat;
-	public double lon;
-	public int cnt;
+	private double lat;
+	private double lon;
+	private int cnt;
+	
+	
+	public RequestBodyClass(double lat, double lon, int cnt) {
+	
+		this.lat = lat;
+		this.lon = lon;
+		this.cnt = cnt;
+	}
+	
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLon() {
+		return lon;
+	}
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
