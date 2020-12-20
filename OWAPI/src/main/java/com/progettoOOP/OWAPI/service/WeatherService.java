b1d2cdfade38f917302e283b9598e09187233e0e
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-20 20:02:44 +0100

    Update WeatherService.java

--- OWAPI/src/main/java/com/progettoOOP/OWAPI/service/WeatherService.java
+++ OWAPI/src/main/java/com/progettoOOP/OWAPI/service/WeatherService.java
@@ -5,19 +5,31 @@ import java.util.ArrayList;
 import com.progettoOOP.OWAPI.model.AbstractCityData;
 
 
-/* @Author Luigi Smargiassi
+/** @author Luigi Smargiassi
  * Interfaccia pubblica del servizio meteo 
  */
 
 public interface WeatherService {
 
-	//metodo astratto per la chiamata alle funzionalità del servizio meteo relative alle condizioni attuali
+	/**
+	 * metodo astratto per la chiamata alle funzionalità del servizio meteo relative alle condizioni attuali
+	 * 
+	 * @param lat
+	 * @param lon
+	 * @param cnt
+	 */
 	public abstract ArrayList<AbstractCityData> actualService(double lat,double lon,int cnt);
 	
-	/*metodo astratto per la chiamata alle funzionalità del servizio meteo relative alle condizioni storiche
+	/** metodo astratto per la chiamata alle funzionalità del servizio meteo relative alle condizioni storiche
 	 * del parametro "type" -> cloud/pressure 
-	 * sul periodo "period" -> misurato in giorni
-    */
+	 *sul periodo "period" -> misurato in giorni
+	 *
+	 * @param period
+	 * @param lat
+	 * @param lon
+	 * @param cnt
+	 * @param type
+         */
 	public abstract ArrayList<AbstractCityData> statService
 		(int period, double lat, double lon, int cnt, String type);
 
