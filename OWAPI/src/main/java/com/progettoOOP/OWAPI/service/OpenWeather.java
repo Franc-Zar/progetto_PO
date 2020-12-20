0fcde9cd840105296b3b76e2220cef0aa406f732
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-20 20:01:47 +0100

    Update OpenWeather.java

--- OWAPI/src/main/java/com/progettoOOP/OWAPI/service/OpenWeather.java
+++ OWAPI/src/main/java/com/progettoOOP/OWAPI/service/OpenWeather.java
@@ -10,24 +10,34 @@ import org.json.JSONObject;
 import com.progettoOOP.OWAPI.util.FileUtilities;
 import com.progettoOOP.OWAPI.model.AbstractCityData;
 
-/* @Author Francesco Zaritto
- * @Author Luigi Smargiassi 
+/** @author Francesco Zaritto
+ *  @author Luigi Smargiassi 
  * 
  * classe public i cui metodi stabiliscono la connessione all'API o all'Archivio e che, dopo la manipolazione dei
  * dati, li restituisce all'utente nel formato dell'applicazione, in funzione delle richieste effettuate
  */
 public class OpenWeather {
 	
-	//key necessaria per chiamare l'API
 	private final static String APIkey=FileUtilities.getFileContent("src/main/resources/APIkey.txt");
 
-/* metodo public che prende in input le coordinate geografiche (lat,lon), il periodo entro il quale definire
+/** metodo public che prende in input le coordinate geografiche (lat,lon), il periodo entro il quale definire
  * i dati (period=0 nel caso di informazioni attuali), il numero totale di città da analizzare (cnt = 1 default)
  * e una Stringa contenente il tipo di richiesta (type = "actual" nel caso di informazioni attuali, "type" = 
  * pressure/cloud nel caso di richieste di statistiche). Restituisce un ArrayList contenente le città cercate
  * e le relative informazioni desiderate
+ * 
+ * @throws IllegalArgumentException
+ * 
+ * @param lat
+ * @param lon
+ * @param period
+ * @param cnt
+ * @param type
+ * 
+ * 
+ * @return informazioni cercate sulle città (sia attuali che storiche)
  */
-	public static ArrayList<AbstractCityData> APIcall(double lat,double lon,int period,int cnt,String type){
+	public static ArrayList<AbstractCityData> APIcall(double lat,double lon,int period,int cnt,String type) throws IllegalArgumentException{
 
 		if(cnt<1||cnt>20) throw new IllegalArgumentException("ERROR: Invalid cities' number");
 		if(lon<-180.0||lon>180.0) throw new IllegalArgumentException("ERROR: Invalid longitude");
@@ -69,11 +79,16 @@ public class OpenWeather {
 		return filteredData;
 	}
 	
-	/* metodo private che prende come parametri un JSONArray contenente i dati ottenuti dalla chiamata all'
-	 * archivio (data), un ArrayList (filteredData), una stringa contenente il tipo di parametro sul quale
-	 * effettuare le statistiche (type = pressure/cloud) e il periodo (period) sul quale calcolare tali statistiche
-	 * (media e varianza) 
-	 */
+/** metodo private che prende come parametri un JSONArray contenente i dati ottenuti dalla chiamata all'
+* archivio (data), un ArrayList (filteredData), una stringa contenente il tipo di parametro sul quale
+* effettuare le statistiche (type = pressure/cloud) e il periodo (period) sul quale calcolare tali statistiche
+* (media e varianza) 
+* 
+* @param data
+* @param filteredData
+* @param type
+* @param period
+*/
 	private static void getStats(JSONArray data, ArrayList<AbstractCityData> filteredData, String type, int period) {
 		
 	if(type.equals("all")) Filters.calculateStats(data, filteredData, period, "cloud","pressure");
