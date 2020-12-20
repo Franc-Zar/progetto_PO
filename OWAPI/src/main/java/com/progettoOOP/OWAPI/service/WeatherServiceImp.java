57835aa34c77678fd2e54b9a4cd6cc35801d5b31
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-20 20:03:17 +0100

    Update WeatherServiceImp.java

--- OWAPI/src/main/java/com/progettoOOP/OWAPI/service/WeatherServiceImp.java
+++ OWAPI/src/main/java/com/progettoOOP/OWAPI/service/WeatherServiceImp.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Service;
 import com.progettoOOP.OWAPI.model.AbstractCityData;
 
-/* @Author Luigi Smargiassi
+/** @author Luigi Smargiassi
  * 
  * classe che implementa l'interfaccia public "WeatherService" 
  */
@@ -12,31 +12,45 @@ import com.progettoOOP.OWAPI.model.AbstractCityData;
 public class WeatherServiceImp implements WeatherService {
 	
 	
-/* metodo public che prende come parametri le coordinate della città (lat,lon) e il numero cnt di città da analizzare.
+/** metodo public che prende come parametri le coordinate della città (lat,lon) e il numero cnt di città da analizzare.
  * Richiama le funzionalità di "APIcall", richiedendo informazioni attuali riguardo nuvolosità e pressione, e 
  * restituendole all'utente	 
+ * 
+ * @param lat
+ * @param lon
+ * @param cnt
+ * 
+ * @return dati forniti da OpenWeather.APIcall
  */
 	@Override
 	public ArrayList<AbstractCityData> actualService(double lat, double lon, int cnt) {
 		try {
 			return OpenWeather.APIcall(lat, lon, 0, cnt, "actual");
-		}catch (Exception e){e.printStackTrace();}
+		}catch (IllegalArgumentException e){e.printStackTrace();}
 		
 		return null;
 	}
 	
 	
-	/* metodo public che prende come parametri le coordinate della città (lat,lon) e il numero (cnt) di città da analizzare,
+	/** metodo public che prende come parametri le coordinate della città (lat,lon) e il numero (cnt) di città da analizzare,
 	 * il periodo sul quale calcolare le statistiche (period) e una stringa contenente il parametro sul quale si 
 	 * vogliono eseguire tali statistiche (type = "cloud"/"pressure")
 	 * Richiama le funzionalità di "APIcall", richiedendo informazioni dallo storico riguardo nuvolosità/pressione,
-	 * entro il periodo specificato, ne calcola media e varianza e le restituisce all'utente	 
+	 * entro il periodo specificato, ne calcola media e varianza e le restituisce all'utente	
+	 * 
+	 * @param period
+     * @param lat
+     * @param lon
+     * @param cnt
+     * @param type
+     * 
+     * @return dati forniti da OpenWeather.APIcall
 	 */
 	@Override
 	public ArrayList<AbstractCityData> statService(int period, double lat, double lon, int cnt, String type) {
 		try {
 			 return OpenWeather.APIcall(lat, lon, period, cnt, type);
-		}catch (Exception e) {e.printStackTrace();}
+		}catch (IllegalArgumentException e) {e.printStackTrace();}
 		return null;
 	}
 
