780aba62ce8bd86240ae3d9c4447ab608dd01c4b
Franc-Zar <75085155+Franc-Zar@users.noreply.github.com>
2020-12-20 20:06:36 +0100

    Update RequestBodyClass.java

--- OWAPI/src/main/java/com/progettoOOP/OWAPI/model/RequestBodyClass.java
+++ OWAPI/src/main/java/com/progettoOOP/OWAPI/model/RequestBodyClass.java
@@ -1,5 +1,16 @@
 package com.progettoOOP.OWAPI.model;
-
+/**
+ * 
+ * @author Luigi Smargiassi
+ *
+ * Classe public che rappresenta il tipo di dato, utilizzato all'interno dell'applicazione, per eseguire
+ * le richieste in POST 
+ * 
+ * costruttore:
+ * @param lat
+ * @param lon
+ * @param cnt
+ */
 public class RequestBodyClass {
 	private double lat;
 	private double lon;
@@ -30,16 +41,5 @@ public class RequestBodyClass {
 	}
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}	
 }
