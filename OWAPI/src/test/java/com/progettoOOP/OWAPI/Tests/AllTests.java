4ae88ea70601b6b9a31719d4b54625b8d91a97b5
Luigi-S <75085145+Luigi-S@users.noreply.github.com>
2020-12-22 11:26:37 +0100

    Update AllTests.java

--- OWAPI/src/test/java/com/progettoOOP/OWAPI/Tests/AllTests.java
+++ OWAPI/src/test/java/com/progettoOOP/OWAPI/Tests/AllTests.java
@@ -1,7 +1,11 @@
-package com.progettoOOP.OWAPI;
+package com.progettoOOP.OWAPI.Tests;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
+
+/**
+* @author Luigi Smargiassi
+*/
 
 @RunWith(JUnitPlatform.class)
 @SelectClasses({ArchiveTest.class,UtilitiesTest.class,TestCityData.class,TestOpenWeather.class})
